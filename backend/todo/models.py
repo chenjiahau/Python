2848bf829d33e9d7deb41f99b4d71da0fd3316cf
chenjiahau <ivan.chen@dlinkcorp.com>
2024-10-21 12:00:21 +0800

    Set UTC and timezone

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     completed = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.title
