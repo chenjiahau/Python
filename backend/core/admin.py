34e9d9f798712be2070c3600114aeb04407cb399
ivanchen <ivan.chen@dlinkcorp.com>
2024-10-30 17:49:29 +0800

    Custom admin UI

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
-from core.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from core import models
 
-# Register your models here.
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    ordering = ['id']
+    list_display = ['email', 'first_name', 'last_name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal Info', {'fields': ('first_name', 'last_name')}),
+        (
+            'Permissions',
+            {'fields': ('is_active', 'is_staff', 'is_superuser')}
+        ),
+        ('Important dates', {'fields': ('last_login',)})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin)
